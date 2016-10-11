615391593a442b67952d436f9ffbd23531d67550
Andrew Bender <andrew@bender.io>
2016-10-10 23:37:43 -0500

    Setup basic bank rendering

--- App.elm
+++ App.elm
@@ -2,6 +2,7 @@ module App exposing (..)
 
 import String
 import List
+import Dict exposing(Dict)
 import Html exposing (Html, button, div, text)
 import Html.App as App
 import Html.Events exposing (onClick)
@@ -14,29 +15,31 @@ main =
 -- MODEL
 
 type alias Letter = Char
+
 type alias Word = List Letter
 type alias LetterTile =
-  { letter: Letter
-  , position: Int
-  , inSentence: Bool
-  }
+  { letter: Letter }
 
 type alias Model =
   { sentence: List Word
-  , positionedLetters: List LetterTile
+  , positionedTiles: Dict Int LetterTile
+  , bankedTiles: Dict Int LetterTile
   }
 
-makeLetterTile : Int -> Letter -> LetterTile
-makeLetterTile index letter =
-  { letter = letter
-  , position = index
-  , inSentence = False
-  }
+makeLetterTile : Letter -> LetterTile
+makeLetterTile letter =
+  { letter = letter }
 
 initialModel : String -> Model
 initialModel sentence =
   { sentence = sentence |> String.words |> List.map String.toList
-  , positionedLetters = sentence |> String.toList |> List.indexedMap makeLetterTile
+  , positionedTiles = Dict.empty
+  , bankedTiles = sentence
+      |> String.filter (\letter -> letter /= ' ')
+      |> String.toList
+      |> List.map makeLetterTile
+      |> List.indexedMap (,)
+      |> Dict.fromList
   }
 
 
@@ -69,6 +72,14 @@ wordSlot : Word -> Html Msg
 wordSlot word =
   div [] (List.map letterSlot word)
 
+renderTile : Int -> LetterTile -> Html Msg
+renderTile index tile =
+  div [] [text (String.fromChar (.letter tile))]
+
 view : Model -> Html Msg
 view model =
-  div [] (List.map wordSlot model.sentence)
+  div
+    []
+    [ div [] (List.map wordSlot model.sentence)
+    , div [] (Dict.values (Dict.map renderTile model.bankedTiles))
+    ]

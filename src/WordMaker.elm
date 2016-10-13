091c9b3df5ec4eebdd6ca4b24b51f90dfe6bec0d
Andrew Bender <andrew@bender.io>
2016-10-13 03:09:35 -0500

    Add ability to move tiles from bank to puzzle

--- src/WordMaker.elm
+++ src/WordMaker.elm
@@ -26,6 +26,8 @@ type alias Model =
   { sentence: List Word
   , positionedTiles: Dict Int LetterTile
   , bankedTiles: Dict Int LetterTile
+  , selectedBankTile: Maybe Int
+  , selectedPositionedTile: Maybe Int
   }
 
 makeLetterTile : Letter -> LetterTile
@@ -43,52 +45,123 @@ initialModel sentence =
       |> List.map makeLetterTile
       |> List.indexedMap (,)
       |> Dict.fromList
+  , selectedBankTile = Nothing
+  , selectedPositionedTile = Nothing
   }
 
 
 -- UPDATE
 
-type Msg = NoOp
+type Msg
+  = ClickBankTile Int
+  | ClickPositionedTile Int
+  | ClearSelected
+  | ClickSlotWithBanked Int Int
+  | NoOp
 
 update : Msg -> Model -> Model
-update msg model = model
+update msg model =
+  case msg of
+    ClickBankTile index ->
+      { model |
+        selectedBankTile = Just index,
+        selectedPositionedTile = Nothing
+      }
 
---type Msg = Increment | Decrement
+    ClickPositionedTile index ->
+      { model |
+        selectedBankTile = Nothing,
+        selectedPositionedTile = Just index
+      }
 
---update : Msg -> Model -> Model
---update msg model =
---  case msg of
---    Increment ->
---      model + 1
+    ClearSelected ->
+      { model |
+        selectedBankTile = Nothing,
+        selectedPositionedTile = Nothing
+      }
 
---    Decrement ->
---      model - 1
+    ClickSlotWithBanked index fromIndex ->
+      case Dict.get fromIndex model.bankedTiles of
+        Just tile ->
+          { model |
+            bankedTiles = model.bankedTiles |> Dict.remove fromIndex,
+            positionedTiles = model.positionedTiles |> Dict.insert index tile
+          }
+        Nothing ->
+          model
+
+    NoOp ->
+      model
 
 
 -- VIEW
 
-letterSlot : Dict Int LetterTile -> Int -> Letter -> Html Msg
-letterSlot positionedTiles index letter =
-  case Dict.get index positionedTiles of
-    Just tile ->
-      renderTile index tile
-    Nothing ->
-      div [class "empty-slot"] []
-
-wordSlot : Dict Int LetterTile -> Int -> Word -> Html Msg
-wordSlot positionedTiles index word =
-  div [class "word"] (List.map (letterSlot positionedTiles index) word)
-
-renderTile : Int -> LetterTile -> Html Msg
-renderTile index tile =
-  div [class "tile"] [text (String.fromChar (.letter tile))]
+type TileType = Banked | Positioned
 
 view : Model -> Html Msg
 view model =
   div
-    [id "main"]
+    [ id "main" ]
     [ h1 [] [text "Solve the Puzzle"]
-    , div [class "puzzle"] (List.indexedMap (wordSlot model.positionedTiles) model.sentence)
+    , div [class "puzzle"] (List.indexedMap (viewWordSlot model) model.sentence)
     , h2 [] [text "Letter Bank"]
-    , div [class "tile-bank"] (Dict.values (Dict.map renderTile model.bankedTiles))
+    , div [class "tile-bank"] (Dict.values (Dict.map (viewTile Banked model.selectedBankTile) model.bankedTiles))
+    , div [] [text (toString model.selectedPositionedTile)]
     ]
+
+viewLetterSlot : Model -> Int -> Letter -> Html Msg
+viewLetterSlot model index letter =
+  let
+    (isBankedSelected, selectedIndex) = selectedBanked model
+  in
+    case Dict.get index model.positionedTiles of
+      Just tile ->
+        viewTile Positioned model.selectedPositionedTile index tile
+      Nothing ->
+        div
+          [ class "empty-slot"
+          , onClick (if isBankedSelected then (ClickSlotWithBanked index selectedIndex) else NoOp)
+          ]
+          []
+
+viewWordSlot : Model -> Int -> Word -> Html Msg
+viewWordSlot model wordIndex word =
+  div [ class "word" ] (List.indexedMap (\index letter -> viewLetterSlot model (index + (calcWordStartIndex model.sentence wordIndex)) letter) word)
+
+viewTile : TileType -> Maybe Int -> Int -> LetterTile -> Html Msg
+viewTile tileType selectedIndex index tile =
+  let
+    selected = isSelected selectedIndex index
+  in
+    div
+      [ classList [ ("tile", True), ("selected", selected) ]
+      , onClick (if selected then (ClearSelected) else if tileType == Banked then (ClickBankTile index) else (ClickPositionedTile index))
+      ]
+      [ text (String.fromChar (.letter tile)) ]
+
+isSelected : Maybe Int -> Int -> Bool
+isSelected selectedIndex index =
+  case selectedIndex of
+    Just value ->
+      value == index
+    Nothing ->
+      False
+
+selectedBanked : Model -> (Bool, Int)
+selectedBanked model =
+  case model.selectedBankTile of
+    Just value -> (True, value)
+    Nothing -> (False, -1)
+
+selectedPositioned : Model -> (Bool, Int)
+selectedPositioned model =
+  case model.selectedPositionedTile of
+    Just value -> (True, value)
+    Nothing -> (False, -1)
+
+calcWordStartIndex : List Word -> Int -> Int
+calcWordStartIndex sentence wordIndex =
+  sentence
+    |> List.take wordIndex
+    |> List.map (List.length)
+    |> List.foldr (+) 0

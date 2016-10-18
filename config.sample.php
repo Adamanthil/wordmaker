61c5f1a96b6cc176fe0fbef33fa69b968ac370ab
Andrew Bender <andrew@bender.io>
2016-10-18 18:41:28 -0500

    Add default user agent reporting and message capability to notify php

--- config.sample.php
+++ config.sample.php
@@ -1,8 +1,13 @@
 <?php
 
 return [
-    'sid' => 'AC86bee4c782af98d35b14d26d7e85028b',
-    'auth_token' => 'token',
-    'from_number' => '+12629999999',
-    'to_number' => '+12629999999'
+	'twilio' => [
+	    'sid' => 'AC86bee4c782af98d35b14d26d7e85028b',
+	    'auth_token' => 'token',
+	    'from_number' => '+12629999999',
+	    'to_number' => '+12629999999'
+	],
+	'useragent' => [
+		'api_key' => 'aaaaa'
+	]
 ];
--- notify.php
+++ notify.php
@@ -1,22 +1,56 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
 
-$config = include('config.php');
+$GLOBALS['config'] = include('config.php');
 
 // Use the REST API Client to make requests to the Twilio REST API
 use Twilio\Rest\Client;
 
-// Your Account SID and Auth Token from twilio.com/console
-$sid = $config['sid'];
-$token = $config['auth_token'];
-$client = new Client($sid, $token);
+if (strtoupper($_SERVER['REQUEST_METHOD']) == 'POST') {
+	if(isset($_POST['message'])) {
+		sendSms($_POST['message']);
+	}
+	else {
+		sendSms(getVisitMessage());
+	}
+}
 
-// Use the client to do fun stuff like send text messages!
-$client->messages->create(
-    // the number you'd like to send the message to
-    $config['to_number'],
-    array(
-        'from' => $config['from_number'],
-        'body' => 'Word Puzzle has been visited!'
-    )
-);
+function sendSms($message) {
+	// Your Account SID and Auth Token from twilio.com/console
+	$sid = $GLOBALS['config']['twilio']['sid'];
+	$token = $GLOBALS['config']['twilio']['auth_token'];
+	$client = new Client($sid, $token);
+
+	// Use the client to do fun stuff like send text messages!
+	$client->messages->create(
+	    // the number you'd like to send the message to
+	    $GLOBALS['config']['twilio']['to_number'],
+	    array(
+	        'from' => $GLOBALS['config']['twilio']['from_number'],
+	        'body' => $message
+	    )
+	);
+}
+
+function getVisitMessage() {
+	return "WordMaker has been visited\nby " . getUserAgentDisplay() . "\nin " . getIPLocationDisplay();
+}
+
+function getUserAgentDisplay() {
+	$userAgentURL = 'https://useragentapi.com/api/v3/json/' . $GLOBALS['config']['useragent']['api_key'] . '/';
+	$userAgent = json_decode(file_get_contents($userAgentURL . urlencode($_SERVER['HTTP_USER_AGENT'])));
+
+	return $userAgent->data->platform_type . ' '
+		. $userAgent->data->browser_name. ' '
+		. $userAgent->data->browser_version . "\nfor "
+		. $userAgent->data->platform_name;
+}
+
+function getIPLocationDisplay() {
+	$ipApiUrl = 'http://ip-api.com/json/';
+	$ipLocation = json_decode(file_get_contents($ipApiUrl . $_SERVER['REMOTE_ADDR']));
+
+	if($ipLocation->status == "success") {
+		return $ipLocation->city . ', ' . $ipLocation->region;
+	}
+}
